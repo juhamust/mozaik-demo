25ef86a5561d2a8a904d759cdff0ad24a378642d
Juha Mustonen <juha.mustonen@iki.fi>
2016-02-29 23:21:55 +0200

    Babysteps

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-require('babel/register')({
-    only: /node_modules\/mozaik[^/]*\/src/
-});
+// require('babel/register')({
+//     _only: /node_modules\/mozaik[^/]*\/src/
+// });
 
-var mozaik = new (require('mozaik'))(require('./config'));
+console.log('moz', require('mozaik'));
 
-mozaik.bus.registerApi('github',  require('mozaik-ext-github/client'));
-mozaik.bus.registerApi('travis',  require('mozaik-ext-travis/client'));
-mozaik.bus.registerApi('weather', require('mozaik-ext-weather/client'));
+import Mozaik from 'mozaik';
+
+
+var mozaik = new Mozaik(require('./config'));
+
+//mozaik.bus.registerApi('github',  require('mozaik-ext-github/client'));
+//mozaik.bus.registerApi('travis',  require('mozaik-ext-travis/client'));
+//mozaik.bus.registerApi('weather', require('mozaik-ext-weather/client'));
 
 mozaik.startServer();
