43d64a5f9ad074ebe4362102c643114cba57b18f
Juha Mustonen <juha.mustonen@iki.fi>
2016-03-12 23:13:47 +0200

    Pass location where to place collected styles

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,10 +5,11 @@ import baseConfig from 'mozaik/webpack.config';
 delete baseConfig.entry.mozaik;
 
 baseConfig.output.path = path.join(__dirname, 'build');
-baseConfig.entry.demo = path.join(__dirname, 'src/App.jsx');
+baseConfig.entry.mozaik = path.join(__dirname, 'src/App.jsx');
 
 baseConfig.plugins.push(new MozaikPlugin({
-  pluginDir: path.join(__dirname, 'node_modules')
+  pluginDir: path.join(__dirname, 'node_modules'),
+  collectedStylus: path.join(__dirname, 'node_modules/mozaik/src/ext/collected.styl')
 }));
 
 module.exports = baseConfig;
